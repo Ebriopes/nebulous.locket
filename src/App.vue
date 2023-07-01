97c564d10ba4155004aa15c414ec8b937182dc14
ebriopes <antonio.vargasrosales@gmail.com>
2023-07-01 16:34:09 -0600

    :art: Chore: set the main structure/style to app component

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,29 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterLink, RouterView } from 'vue-router';
+import NavBar from './components/NavBar.vue';
 </script>
 
 <template>
-  <RouterView />
+  <header id="header">
+    <NavBar />
+  </header>
+
+  <main class="content">
+    <RouterView />
+    <div class="test"></div>
+  </main>
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
+$navbar-height: 58px;
+
+#header {
+  width: 100%;
+}
+
+.content {
+  overflow: auto;
+  min-height: calc(100dvh - $navbar-height);
+  max-height: calc(100dvh - $navbar-height);
+}
 </style>
