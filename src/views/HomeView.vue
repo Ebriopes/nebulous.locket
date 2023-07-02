b38bbdb4f0f6ba00b597a53bf0c5a6cd2cfd8f82
ebriopes <antonio.vargasrosales@gmail.com>
2023-07-01 20:26:51 -0600

    :sparkles: Style: add components to Home

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,17 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import MoviesList from '@/components/MoviesList.vue';
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="container card my-5">
+    <div class="row row-gap-4 card-body">
+      <section class="col col-12 filter-container card">
+        <div class="card-body"></div>
+      </section>
+
+      <section class="col col-12 filter-container card">
+        <MoviesList class="card-body" />
+      </section>
+    </div>
+  </div>
 </template>
